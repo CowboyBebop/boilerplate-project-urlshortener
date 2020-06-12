beb32f70592091f6f97daf81d05d26cecb4008c7
RebornInferno <antonpirogov@mail.ru>
2020-06-12 20:59:24 +0300

    new url and lookup done?

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var dns = require('dns');
+var crypto = require('crypto');
 
 var cors = require('cors');
+const { URL } = require('url');
 
 var app = express();
 
@@ -12,12 +15,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(ENV['MONGO_URI']);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+/* In case body-parser is still needed, break this glass
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+*/
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,12 +32,74 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+//set up schemas n' shit
+
+const shortUrlSchema = mongoose.Schema({
+  hashedUrl: {type: String, required: true},
+  originalUrl: {type: String, required: true}
+
+})
+
+const ShortUrlModel = mongoose.model("Shortened Url", shortUrlSchema);
+
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+//get the new shorturl
+app.post("/api/shorturl/new", function (req, res) {
+  
+  let newUrl = req.body
+
+
+  //check if it's a valid url
+  if (dns.lookup(newUrl, (err) => {
+    //if not send {"error":"invalid URL"} back
+    if(err) return res.json({"error":"invalid URL"});
+  }))
+
+  //otherwise create hash and check if it already exists in the db
+  let hash = crypto.createHash('sha1').update(newUrl).digest('base64')
+
+  ShortUrlModel.findOne({hashedUrl: hash}, (err,data) => {
+    if(err) return console.log(err);
+    
+    //if it doesn't exist, add it to the db
+    if(!data) 
+    {
+      let urlDoc = ShortUrlModel({originalUrl: newUrl, hashedUrl: hash})
+      urlDoc.save();
+    }
+    //otherwise don't do anything
+  })
+
+  //return the object
+  res.json({"original_url": newUrl ,"short_url": hash}) 
+
+});
+
+app.get("/api/shorturl/:short_url", function (req, res) {
+  
+  let searchShortenedUrl = req.params.short_url;
+
+  //check for the short url
+  ShortUrlModel.findOne({hashedUrl: searchShortenedUrl}, (err,data) => {
+    if(err) return console.log(err);
+    
+    if(data) 
+    {
+      //if it exists then redirect
+      res.redirect(data.originalUrl)
+    }
+    else
+    {
+      //if not send invalid
+      res.json({"error":"invalid URL"});
+    }
+  })
+
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
